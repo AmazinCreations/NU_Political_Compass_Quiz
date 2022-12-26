3de67e9d00aa1624a1e6e3b96ff38a975349739e
Greddygrim <gautreddy94@gmail.com>
2022-12-26 13:18:14 -0800

    initial

---
+++ questions.js
@@ -0,0 +1,579 @@
+questions = [
+  {
+    id: 0,
+    question: "A nation-state is defined by its?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "People & heritage ",
+        value: [2, 2],
+      },
+      {
+        text: "History & supremacy",
+        value: [2, -2],
+      },
+      {
+        text: "Founding ideas & civic constitution",
+        value: [-2, 2],
+      },
+      {
+        text: " Democratic values & inclusiveness",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 1,
+    question: "A nation-state’s borders must be?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "Secure & strictly regulated",
+        value: [2, 2],
+      },
+      {
+        text: "Secure & expanding",
+        value: [2, -2],
+      },
+      {
+        text: "Secure & open to international immigration",
+        value: [-2, 2],
+      },
+      {
+        text: "Open & welcoming",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "People of a nation-state must share a common?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "Ethnic Heritage ",
+        value: [2, 2],
+      },
+      {
+        text: "Allegiance to the nation-state ",
+        value: [2, -2],
+      },
+      {
+        text: "Official language",
+        value: [-2, 2],
+      },
+      {
+        text: "Democratic worldview ",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: " A nation-state must prioritize the interests of the?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "Majority",
+        value: [2, 2],
+      },
+      {
+        text: "Governing & military authorities",
+        value: [2, -2],
+      },
+      {
+        text: "Individual ",
+        value: [-2, 2],
+      },
+      {
+        text: "Minority",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "A nation-state’s welfare support system should prioritize?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "Its law-abiding people",
+        value: [2, 2],
+      },
+      {
+        text: "Its military & public servants ",
+        value: [2, -2],
+      },
+      {
+        text: "Those most in need",
+        value: [-2, 2],
+      },
+      {
+        text: " Democratic values & inclusiveness",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Immigration into a nation-state should depend on?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "Shared blood with the nation",
+        value: [2, 2],
+      },
+      {
+        text: "Years of service to the nation",
+        value: [2, -2],
+      },
+      {
+        text: "Assimilation of language & culture",
+        value: [-2, 2],
+      },
+      {
+        text: "Shared democratic values",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "One’s citizenship to the nation-state should depend on?",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "The parent’s nationality  ",
+        value: [2, 2],
+      },
+      {
+        text: "Birthright citizenship",
+        value: [2, -2],
+      },
+      {
+        text: "Naturalization & assimilation",
+        value: [-2, 2],
+      },
+      {
+        text: "Living within its territories",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: " A nation must have ______ to defend itself.",
+    effects: { right: 1 },
+    answers: [
+      {
+        text: "National sovereignty & self-determination",
+        value: [2, 2],
+      },
+      {
+        text: "A colonial empire",
+        value: [2, -2],
+      },
+      {
+        text: "Free trade & free markets",
+        value: [-2, 2],
+      },
+      {
+        text: "Equal rights for all",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "A nation which practices ____ best serves their people.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Protectionism",
+        value: [2, 2],
+      },
+      {
+        text: "Colonization",
+        value: [2, -2],
+      },
+      {
+        text: "Free Markets",
+        value: [-2, 2],
+      },
+      {
+        text: "Open Borders",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "Racial diversity is a nation’s ____.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Division",
+        value: [2, 2],
+      },
+      {
+        text: "Conquest",
+        value: [2, -2],
+      },
+      {
+        text: "Privelege",
+        value: [-2, 2],
+      },
+      {
+        text: "Strength",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "A(n) _____ government best defends the will of its citizens.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Nationalist",
+        value: [2, 2],
+      },
+      {
+        text: "Imperialist",
+        value: [2, -2],
+      },
+      {
+        text: "Liberal democratic",
+        value: [-2, 2],
+      },
+      {
+        text: "Globalist",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "Representatives in a government should be ______.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Those most meritocratic",
+        value: [2, 2],
+      },
+      {
+        text: "Those most ambitious",
+        value: [2, -2],
+      },
+      {
+        text: "Those most voted for ",
+        value: [-2, 2],
+      },
+      {
+        text: "Globalist",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "The nation-state should accept immigration from ____.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Those from their ethnic background",
+        value: [2, 2],
+      },
+      {
+        text: "Slaves/indentured servants",
+        value: [2, -2],
+      },
+      {
+        text: "Those who can assimilate",
+        value: [-2, 2],
+      },
+      {
+        text: "All peoples",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "The nation-state’s economic policy must benefit _____ foremost.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Its citizens",
+        value: [2, 2],
+      },
+      {
+        text: "The state’s treasury",
+        value: [2, -2],
+      },
+      {
+        text: "Businesses",
+        value: [-2, 2],
+      },
+      {
+        text: "The world",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 14,
+    question:
+      "The nation-state must ____ foreign cultures within its national boundaries.",
+    effects: { right: -1 },
+    answers: [
+      {
+        text: "Forbid",
+        value: [2, 2],
+      },
+      {
+        text: "Subjugate",
+        value: [2, -2],
+      },
+      {
+        text: "Assimilate",
+        value: [-2, 2],
+      },
+      {
+        text: "Promote",
+        value: [-2, -2],
+      },
+    ],
+  },
+  {
+    id: 15,
+    question:
+      "The nation-state must enact policies which promote ______ values.",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "Traditional family",
+        value: [2, 0],
+      },
+      {
+        text: "Conservative",
+        value: [1, 0],
+      },
+      {
+        text: "Liberal",
+        value: [-1, 0],
+      },
+      {
+        text: "Egalitarian",
+        value: [-2, 0],
+      },
+    ],
+  },
+  {
+    id: 16,
+    question: "Pornography and prostitution should be",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "Stomped out",
+        value: [2, 0],
+      },
+      {
+        text: "Strictly regulated",
+        value: [1, 0],
+      },
+      {
+        text: "Publicly available",
+        value: [-1, 0],
+      },
+      {
+        text: "Publicly promoted",
+        value: [-2, 0],
+      },
+    ],
+  },
+  {
+    id: 17,
+    question: "Censorship is most appropriate when ?",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "For indecency & degeneracy",
+        value: [2, 0],
+      },
+      {
+        text: "For criticisms of the state",
+        value: [1, 0],
+      },
+      {
+        text: "Never",
+        value: [-1, 0],
+      },
+      {
+        text: "For politically incorrect ideas",
+        value: [-2, 0],
+      },
+    ],
+  },
+  {
+    id: 18,
+    question: "The nation-state must serve the needs of the?",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "People",
+        value: [2, 0],
+      },
+      {
+        text: "State",
+        value: [1, 0],
+      },
+      {
+        text: "Individual",
+        value: [-1, 0],
+      },
+      {
+        text: "Oppressed",
+        value: [-2, 0],
+      },
+    ],
+  },
+  {
+    id: 19,
+    question: "A nation should enact policies that help women _____.",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "Raise many children",
+        value: [2, 0],
+      },
+      {
+        text: "Contribute to the state",
+        value: [1, 0],
+      },
+      {
+        text: "Achieve equality",
+        value: [-1, 0],
+      },
+      {
+        text: "Achieve sexual liberation",
+        value: [-2, 0],
+      },
+    ],
+  },
+  {
+    id: 20,
+    question: "A nation must prioritize the needs of?",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "Its law-abiding citizens",
+        value: [0, 2],
+      },
+      {
+        text: "The general public",
+        value: [0, 1],
+      },
+      {
+        text: "Minority groups",
+        value: [0, -1],
+      },
+      {
+        text: "The world ",
+        value: [0, -2],
+      },
+    ],
+  },
+  {
+    id: 21,
+    question: "Nation-states were created for _____.",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "A common people",
+        value: [0, 2],
+      },
+      {
+        text: "A civic identity",
+        value: [0, 1],
+      },
+      {
+        text: "Anyone living there",
+        value: [0, -1],
+      },
+      {
+        text: "The people of the world",
+        value: [0, -2],
+      },
+    ],
+  },
+  {
+    id: 22,
+    question: "The nation-state should protect its people most from?",
+    effects: { auth: 1 },
+    answers: [
+      {
+        text: "Demographic replacement",
+        value: [0, 2],
+      },
+      {
+        text: "Economic exploitation",
+        value: [0, 1],
+      },
+      {
+        text: "Racism & intolerance",
+        value: [0, -1],
+      },
+      {
+        text: "Restrictive immigration policies",
+        value: [0, -2],
+      },
+    ],
+  },
+  {
+    id: 23,
+    question: "The nation-state should _____ other nation-states.",
+    effects: { auth: -1 },
+    answers: [
+      {
+        text: "Work diplomatically with",
+        value: [0, 2],
+      },
+      {
+        text: "Isolate from",
+        value: [0, 1],
+      },
+      {
+        text: "Assimilate",
+        value: [0, -1],
+      },
+      {
+        text: "Integrate with",
+        value: [0, -2],
+      },
+    ],
+  },
+  {
+    id: 24,
+    question: "A nation-state must proactively promote _____.",
+    effects: { auth: -1 },
+    answers: [
+      {
+        text: "Its national identity",
+        value: [0, 2],
+      },
+      {
+        text: "Its patriotism",
+        value: [0, 1],
+      },
+      {
+        text: "Conquest",
+        value: [0, -1],
+      },
+      {
+        text: "Multiculturalism",
+        value: [0, -2],
+      },
+    ],
+  },
+];
